6ffb913fe245ded2144989483c40701b47144286
Marc Pont <mpont91@gmail.com>
2023-09-04 08:55:09 +0200

    Configure for vercel

--- src-ssr/server.js
+++ src-ssr/server.js
@@ -53,14 +53,23 @@ export const create = ssrCreate((/* { ... } */) => {
  * For production, you can instead export your
  * handler for serverless use or whatever else fits your needs.
  */
-export const listen = ssrListen(async ({ app, port, isReady }) => {
-  await isReady()
-  return app.listen(port, () => {
-    if (process.env.PROD) {
-      console.log('Server listening at port ' + port)
-    }
-  })
-})
+export async function listen({ app, port, isReady, ssrHandler }) {
+  if (process.env.DEV) {
+    await isReady();
+    return await app.listen(port, () => {
+      if (process.env.PROD) {
+        console.log('Server listening at port ' + port);
+      }
+    });
+  } else {
+    // in production
+    // "ssrHandler" is a prebuilt handler which already
+    // waits for all the middlewares to run before serving clients
+
+    // whatever you return here is equivalent to module.exports.<key> = <value>
+    return { handler: ssrHandler };
+  }
+}
 
 /**
  * Should close the server and free up any resources.
